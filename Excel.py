74b9e601643618e3cbd32df888097c417f914eb0
taewook <tawook77@naver.om>
2018-05-09 16:35:20 +0900

    number read / using max row&col size
    1. number read
       QTableWidget can't update int
       using is_number() func.
       number change to string and update to QTable Widget
    
    2. Using Max row & Col
       when user load xlsx file.
       maximum row and col get first
       and then, update QTableWidget Size

--- Excel.py
+++ Excel.py
@@ -11,6 +11,14 @@ form_class = uic.loadUiType("Excel.ui")[0]
 MAX_COL_SIZE = 10
 MAX_ROW_SIZE = 10
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super(QMainWindow, self).__init__()
@@ -22,43 +30,61 @@ class MyWindow(QMainWindow, form_class):
         self.tableWidget.setColumnCount(MAX_COL_SIZE)
         self.tableWidget.setRowCount(MAX_ROW_SIZE)
         
-        self.tableWidget.setHorizontalHeaderLabels(['A','B','C','D','E'])
+        self.tableWidget.setHorizontalHeaderLabels(['A','B','C','D','E']) 
         
         self.file_name = [] #Excel file name
         self.wb = [] #work book
         self.ws = [] #work sheet
-        
-        
-        #self.tableWidget.setItem(0,0, QTableWidgetItem("aaa"))
-        #print(QTableWidgetItem("aaa").text())
-        
+       
         
     def Load_btn_clicked(self):
         file_dlg = QFileDialog()
         self.file_name = file_dlg.getOpenFileName(self, 'Open file', os.getcwd(), "Excel files (*.xlsx)")
         print (self.file_name)
-        self.wb = openpyxl.load_workbook(self.file_name[0])
+        self.wb = openpyxl.load_workbook(self.file_name[0]) # data_only=False is deafault value
+                                                            # data_only=True read just result of function
+        self.wb.guess_types = True #type guess enable
         self.ws = self.wb.active
+        
+        global MAX_ROW_SIZE
+        global MAX_COL_SIZE
+
+        MAX_ROW_SIZE = self.ws.max_row
+        MAX_COL_SIZE = self.ws.max_column
+        
+        #change size from sheet size
+        self.tableWidget.setColumnCount(MAX_COL_SIZE)
+        self.tableWidget.setRowCount(MAX_ROW_SIZE)
+        #######
+        ## TODO : From MAX SIZE need to update tableWidget.setHorizontalHeaderLabels ( List of label )
+        #######
         
         for row in range(1, MAX_ROW_SIZE+1):
             for col in range(1, MAX_COL_SIZE+1):
                 cell_txt = self.ws.cell(column = col , row = row).value
-                self.tableWidget.setItem(row-1,col-1, QTableWidgetItem(cell_txt))
+                #print(cell_txt) #for debug
+                if cell_txt is not None:
+                    if(is_number(cell_txt)): #nubmer
+                        self.tableWidget.setItem( row-1,col-1, QTableWidgetItem( str(cell_txt) ) )
+                    else:
+                        self.tableWidget.setItem( row-1,col-1, QTableWidgetItem( cell_txt ) )
                 
-        # ws['A2'] = 'OK'
-        # wb.save('test.xls')x
-        
         
     def Save_btn_clicked(self):
         #print(self.tableWidget.itemAt(1,1).text())
         cell_txt = self.tableWidget.currentItem().text()
+        global MAX_ROW_SIZE
+        global MAX_COL_SIZE
         
         for row in range(1, MAX_ROW_SIZE+1):
             for col in range(1, MAX_COL_SIZE+1):
                 item = self.tableWidget.item(row-1,col-1)
                 if item is not None: #QtableWidget item Exist
                     cell_txt = self.tableWidget.item(row-1,col-1).text()
-                    self.ws.cell(column = col , row = row, value = cell_txt)
+                    if(is_number(cell_txt)): #nubmer
+                        self.ws.cell(column = col , row = row, value = cell_txt).number_format
+                    else: #text
+                        self.ws.cell(column = col , row = row, value = cell_txt)
                 else: #Need to make QtableWidget item 
                     self.ws.cell(column = col , row = row, value = "")
         
@@ -87,12 +113,6 @@ class MyWindow(QMainWindow, form_class):
         else: #Need to make QtableWidget item 
             self.tableWidget.setItem(cur_row,cur_col, QTableWidgetItem("TEST"))
         
-        
-        
-		
-		
-	
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
